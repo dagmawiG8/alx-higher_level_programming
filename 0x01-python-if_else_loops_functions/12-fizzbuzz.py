a8c54867f2a8ae99d80305ac99ce964d3283440f
dagmawiG8 <dagiwold@gmail.com>
2021-11-26 12:33:40 -0800

    correction

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,8 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
-            print("Fizz")
+            print("Fizz", end="")
         if i % 5 == 0:
-            print("Buzz")
+            print("Buzz", end="")
         if i % 3 != 0 and i % 5 != 0:
-            print("{:d} ".format(i))
+            print("{:d} ".format(i), end="")
