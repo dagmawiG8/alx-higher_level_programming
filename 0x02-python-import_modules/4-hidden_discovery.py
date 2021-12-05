c35d13d33c3aaa6ae60b9ce755a58ab7557c1b05
dagmawiG8 <dagiwold@gmail.com>
2021-12-05 09:08:06 -0800

    doing imports

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4 as hiddenfile
+import sys
+
+if __name__ != "__main__":
+    exit()
+
+for hiddennames in hiddenfile:
+    if hiddennames[0:2] != "__":
+        print("{}".format(hiddennames))
