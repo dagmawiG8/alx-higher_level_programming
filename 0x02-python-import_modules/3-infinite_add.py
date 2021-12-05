dd4716535112a501171255a728a7cf1962880de0
dagmawiG8 <dagiwold@gmail.com>
2021-12-05 03:54:58 -0800

    adding arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ = "__main__":
-    i = 0;
-    result = 0;
-
-    for argument in sys.argv:
-        if i != 0:
-            result += int(argument)
-        else:
-            i++;
-
-    print("{:d}".format(result))
-else:
+if __name__ != "__main__":
     exit();
+
+i = 0;
+result = 0;
+    
+for argument in sys.argv:
+    if i != 0:
+        result += int(argument)
+    else:
+        i++;
+
+print("{:d}".format(result))
